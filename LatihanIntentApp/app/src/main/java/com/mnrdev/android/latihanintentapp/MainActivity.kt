2e3dbb85b5127953d487135fa0523ef8cd487789
MNRDev16 <nurroziqin.111@gmail.com>
2021-08-19 16:58:09 +0700

    add Action Onclick on btn_move_activity a add empty Activity with name MoveActivity

--- LatihanIntentApp/app/src/main/java/com/mnrdev/android/latihanintentapp/MainActivity.kt
+++ LatihanIntentApp/app/src/main/java/com/mnrdev/android/latihanintentapp/MainActivity.kt
@@ -2,10 +2,24 @@ package com.mnrdev.android.latihanintentapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnMoveActivity : Button = findViewById(R.id.btn_moveActivity)
+
+        btnMoveActivity.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id){
+            R.id.btn_moveActivity ->{
+
+            }
+        }
     }
 }
