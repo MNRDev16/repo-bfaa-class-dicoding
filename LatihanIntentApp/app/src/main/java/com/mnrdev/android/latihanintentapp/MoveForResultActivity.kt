8e846b54dbec47cab390efdb5da17f9ede8b7eb0
MNRDev16 <nurroziqin.111@gmail.com>
2021-08-19 20:26:44 +0700

    add variable to coonect component

--- LatihanIntentApp/app/src/main/java/com/mnrdev/android/latihanintentapp/MoveForResultActivity.kt
+++ LatihanIntentApp/app/src/main/java/com/mnrdev/android/latihanintentapp/MoveForResultActivity.kt
@@ -2,10 +2,19 @@ package com.mnrdev.android.latihanintentapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.RadioGroup
 
 class MoveForResultActivity : AppCompatActivity() {
+    private lateinit var btnPilih : Button
+    private lateinit var rgNumber : RadioGroup
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_move_for_result)
+
+        btnPilih = findViewById(R.id.btn_choose_number)
+        rgNumber = findViewById(R.id.rg_number)
+        
     }
 }
