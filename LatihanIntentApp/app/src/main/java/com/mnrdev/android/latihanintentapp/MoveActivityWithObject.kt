c6aff84c8e6576e21e957bec1cfe78ceeea69a20
MNRDev16 <nurroziqin.111@gmail.com>
2021-08-19 18:15:11 +0700

    received data Parcelable as Person and display it in tv_received_object

--- LatihanIntentApp/app/src/main/java/com/mnrdev/android/latihanintentapp/MoveActivityWithObject.kt
+++ LatihanIntentApp/app/src/main/java/com/mnrdev/android/latihanintentapp/MoveActivityWithObject.kt
@@ -2,10 +2,24 @@ package com.mnrdev.android.latihanintentapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MoveActivityWithObject : AppCompatActivity() {
+
+    companion object{
+        const val EXTRA_OBJECT = "extra_object"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_move_with_object)
+
+        val tvReceiveObject : TextView = findViewById(R.id.tv_received_object)
+
+        val receiveObject = intent.getParcelableExtra<Person>(EXTRA_OBJECT) as Person
+        val text = "Nama : ${receiveObject.name.toString()} /n" +
+                "Email : ${receiveObject.Email}/n" +
+                "Age : ${receiveObject.age.toString()} /n" +
+                "Location : ${receiveObject.location}"
+        tvReceiveObject.text = text
     }
 }
