b96e7d6236c865d19cd200d6002dbf5ef4e84b5e
MNRDev16 <nurroziqin.111@gmail.com>
2021-08-21 21:09:57 +0700

    add textview and button to code

--- MyFlexibleFragment/app/src/main/java/com/mnrdev/android/myflexiblefragment/DetailCategoryFragment.kt
+++ MyFlexibleFragment/app/src/main/java/com/mnrdev/android/myflexiblefragment/DetailCategoryFragment.kt
@@ -3,10 +3,18 @@ package com.mnrdev.android.myflexiblefragment
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.TextureView
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 
 class DetailCategoryFragment : Fragment() {
+
+    lateinit var tvCategoryName : TextView
+    lateinit var tvCategoryDescription : TextView
+    lateinit var btnProfil : Button
+    lateinit var btnShowDialog : Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,5 +24,12 @@ class DetailCategoryFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_detail_category, container, false)
     }
 
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        tvCategoryDescription = view.findViewById(R.id.tv_category_description)
+        tvCategoryName = view.findViewById(R.id.tv_category_name)
+        btnProfil = view.findViewById(R.id.btn_profil)
+        btnShowDialog = view.findViewById(R.id.btn_show_dialog)
+    }
+    
 }
