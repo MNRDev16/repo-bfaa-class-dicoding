161a325c5c105cc4c664f7c971da8cb880641dbf
MNRDev16 <nurroziqin.111@gmail.com>
2021-08-22 15:43:36 +0700

    instantiate view button, radio group, and radio button to code

--- MyFlexibleFragment/app/src/main/java/com/mnrdev/android/myflexiblefragment/OptionDialogFragment.kt
+++ MyFlexibleFragment/app/src/main/java/com/mnrdev/android/myflexiblefragment/OptionDialogFragment.kt
@@ -5,28 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [OptionDialogFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class OptionDialogFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    private lateinit var rgOption :RadioGroup
+    private lateinit var rbSaf : RadioButton
+    private lateinit var rbMou : RadioButton
+    private lateinit var rbDav : RadioButton
+    private lateinit var rbLvg : RadioButton
+    private lateinit var btnChoose : Button
+    private lateinit var btnClose : Button
+    private var optionDialogListener : OnOptionDialogListener? = null
+
+    interface OnOptionDialogListener {
+
     }
 
     override fun onCreateView(
@@ -37,23 +32,14 @@ class OptionDialogFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_option_dialog, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment OptionDialogFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            OptionDialogFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        rgOption = view.findViewById(R.id.rg_option)
+        rbDav = view.findViewById(R.id.rb_dav)
+        rbLvg = view.findViewById(R.id.rb_lvg)
+        rbMou = view.findViewById(R.id.rb_mou)
+        rbSaf = view.findViewById(R.id.rb_saf)
+        btnChoose = view.findViewById(R.id.btn_choose)
+        btnClose = view.findViewById(R.id.btn_close)
     }
 }
