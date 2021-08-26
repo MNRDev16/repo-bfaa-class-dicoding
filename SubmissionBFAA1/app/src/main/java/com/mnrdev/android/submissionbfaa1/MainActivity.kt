e4d4e50536f2f07a0788c4be0ddc4396e63c3349
MNRDev16 <nurroziqin.111@gmail.com>
2021-08-26 16:28:44 +0700

    adjust layout make viewBinding in MainActivity

--- SubmissionBFAA1/app/src/main/java/com/mnrdev/android/submissionbfaa1/MainActivity.kt
+++ SubmissionBFAA1/app/src/main/java/com/mnrdev/android/submissionbfaa1/MainActivity.kt
@@ -2,10 +2,18 @@ package com.mnrdev.android.submissionbfaa1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mnrdev.android.submissionbfaa1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainVewBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainVewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainVewBinding.root)
+
+
+
     }
 }
