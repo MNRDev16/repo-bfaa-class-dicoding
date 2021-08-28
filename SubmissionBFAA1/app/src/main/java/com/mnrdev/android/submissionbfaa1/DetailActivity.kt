992f613e89f20f151edcfb60c0338e99e567c765
MNRDev16 <nurroziqin.111@gmail.com>
2021-08-28 21:49:56 +0700

    show extraUser to layout and fix some code

--- SubmissionBFAA1/app/src/main/java/com/mnrdev/android/submissionbfaa1/DetailActivity.kt
+++ SubmissionBFAA1/app/src/main/java/com/mnrdev/android/submissionbfaa1/DetailActivity.kt
@@ -2,8 +2,13 @@ package com.mnrdev.android.submissionbfaa1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.mnrdev.android.submissionbfaa1.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
+
+    private lateinit var detailViewBinding : ActivityDetailBinding
 
     companion object{
         const val EXTRA_USER = "extra_user"
@@ -11,5 +16,20 @@ class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+
+        val user = intent.getParcelableExtra<User>(EXTRA_USER)
+
+        Glide.with(this)
+            .load(user?.avatar)
+            .apply(RequestOptions().override(100,100))
+            .into(detailViewBinding.imageUser)
+        detailViewBinding.apply {
+            tvUserName.text = user?.userName
+            tvCompany.text = user?.company
+            tvLocation.text = user?.location
+            tvNumFollower.text = user?.follower
+            tvNumFollowing.text = user?.following
+            tvNumRepository.text = user?.repository
+        }
     }
 }
