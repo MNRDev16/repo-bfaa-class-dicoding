fe26543d7752a933928aaeb373167997739b2792
MNRDev16 <nurroziqin.111@gmail.com>
2021-08-28 23:21:01 +0700

    make action share and its implementation

--- SubmissionBFAA1/app/src/main/java/com/mnrdev/android/submissionbfaa1/DetailActivity.kt
+++ SubmissionBFAA1/app/src/main/java/com/mnrdev/android/submissionbfaa1/DetailActivity.kt
@@ -1,7 +1,10 @@
 package com.mnrdev.android.submissionbfaa1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.mnrdev.android.submissionbfaa1.databinding.ActivityDetailBinding
@@ -15,7 +18,8 @@ class DetailActivity : AppCompatActivity() {
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        detailViewBinding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(detailViewBinding.root)
 
         val user = intent.getParcelableExtra<User>(EXTRA_USER)
 
@@ -31,5 +35,27 @@ class DetailActivity : AppCompatActivity() {
             tvNumFollowing.text = user?.following
             tvNumRepository.text = user?.repository
         }
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            title = user?.userID
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.action_share){
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.type = "text/plain"
+            var shareBody =" Your body"
+            var shareSub = "Your Sub"
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT,shareBody)
+            shareIntent.putExtra(Intent.EXTRA_TEXT,shareSub)
+            startActivity(Intent.createChooser(shareIntent,"Sahe using"))
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
