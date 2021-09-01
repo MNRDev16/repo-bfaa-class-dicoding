d9379b5f28609bcbade6c1d335f1a4eb3e9f2dcb
MNRDev16 <nurroziqin.111@gmail.com>
2021-09-01 14:43:47 +0700

    theme modification

--- SubmissionBFAA1/app/src/main/java/com/mnrdev/android/submissionbfaa1/SplashActivity.kt
+++ SubmissionBFAA1/app/src/main/java/com/mnrdev/android/submissionbfaa1/SplashActivity.kt
@@ -4,13 +4,24 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.mnrdev.android.submissionbfaa1.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var splashViewBinding : ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        splashViewBinding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(splashViewBinding.root)
 
         supportActionBar?.hide()
+        Glide.with(this)
+            .load(R.drawable.github_128_black)
+            .apply(RequestOptions().override(128,128))
+            .into(splashViewBinding.iconSplash)
 
         Handler(mainLooper).postDelayed({
             val home = Intent(this,MainActivity::class.java)
