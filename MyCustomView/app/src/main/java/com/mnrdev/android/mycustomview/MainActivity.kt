b426375fa6a6df0e78e63df88fccc463e6119105
MNRDev16 <nurroziqin.111@gmail.com>
2021-09-10 17:17:35 +0700

    costuming myEditText, place in main activity

--- MyCustomView/app/src/main/java/com/mnrdev/android/mycustomview/MainActivity.kt
+++ MyCustomView/app/src/main/java/com/mnrdev/android/mycustomview/MainActivity.kt
@@ -2,10 +2,43 @@ package com.mnrdev.android.mycustomview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var myButton: MyButton
+    private lateinit var myEditText: MyEditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        myButton = findViewById(R.id.btn_submit)
+        myEditText = findViewById(R.id.edt_my_edit_text)
+
+        setMyButtonEnable()
+
+        myEditText.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                setMyButtonEnable()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                TODO("Not yet implemented")
+            }
+
+        })
+        myButton.setOnClickListener { Toast.makeText(this,myEditText.text,Toast.LENGTH_SHORT).show()}
+    }
+
+    private fun setMyButtonEnable(){
+        val result = myEditText.text
+        myButton.isEnabled = result != null && result.toString().isNotEmpty()
     }
 }
--- MyCustomView/app/src/main/java/com/mnrdev/android/mycustomview/MyEditText.kt
+++ MyCustomView/app/src/main/java/com/mnrdev/android/mycustomview/MyEditText.kt
@@ -3,6 +3,8 @@ package com.mnrdev.android.mycustomview
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.drawable.Drawable
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
@@ -14,23 +16,39 @@ class MyEditText : AppCompatEditText, View.OnTouchListener {
     internal lateinit var mClearButtonImage : Drawable
 
     constructor(context : Context): super(context){
-
+        init()
     }
     constructor(context: Context, attrs : AttributeSet): super(context,attrs){
-
+        init()
     }
     constructor(context: Context, attrs: AttributeSet, defStyleAttr : Int): super(context, attrs, defStyleAttr){
-
-
+        init()
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
+        hint = "Masukan nama anda"
+        textAlignment = View.TEXT_ALIGNMENT_VIEW_START
     }
 
     private fun init(){
         mClearButtonImage = ResourcesCompat.getDrawable(resources,R.drawable.ic_baseline_close_24,null) as Drawable
         setOnTouchListener(this)
+
+        addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if(s.toString().isEmpty()) showClearButton() else hideClearButton()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+        })
     }
 
     private fun showClearButton(){
