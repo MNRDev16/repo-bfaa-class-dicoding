dbf8cf4c9e361cde489bec9b87f847f3cffd6a5c
MNRDev16 <nurroziqin.111@gmail.com>
2021-09-17 18:49:33 +0700

    way to TabLayout with one Fragment

--- MyTabLayout/app/src/main/java/com/mnrdev/android/mytablayout/SectionsPagerAdapter.kt
+++ MyTabLayout/app/src/main/java/com/mnrdev/android/mytablayout/SectionsPagerAdapter.kt
@@ -6,15 +6,18 @@ import androidx.viewpager2.adapter.FragmentStateAdapter
 
 class SectionsPagerAdapter(activity: AppCompatActivity): FragmentStateAdapter(activity) {
     override fun getItemCount(): Int {
-        return 2
+        return 3
     }
 
     override fun createFragment(position: Int): Fragment {
-        var fragment : Fragment? = null;
-        when(position){
-            0 -> fragment = HomeFragment()
-            1 -> fragment = ProfileFragment()
-        }
-        return fragment as Fragment
+        //< ganti fragment>
+        // val fragment : Fragment? = null
+        // when (position){
+        //      0 -> HomeFragment()
+        //      1 -> ProfileFragment()
+        // }
+        // return fragment as Fragment
+
+        return HomeFragment.newInstance(position + 1)
     }
 }
