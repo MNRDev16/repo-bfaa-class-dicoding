62c86b87495e708e0eef497389923876fab68b3e
MNRDev16 <nurroziqin.111@gmail.com>
2021-09-22 11:35:28 +0700

    fix library and gradle version

--- SubmissionBFAA2/app/src/main/java/com/mnrdev/android/submissionbfaa2/MainActivity.kt
+++ SubmissionBFAA2/app/src/main/java/com/mnrdev/android/submissionbfaa2/MainActivity.kt
@@ -2,10 +2,14 @@ package com.mnrdev.android.submissionbfaa2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mnrdev.android.submissionbfaa2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
     }
 }
