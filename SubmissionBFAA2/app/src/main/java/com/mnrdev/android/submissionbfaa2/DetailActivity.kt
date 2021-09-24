2fce5c9e2e514cb3a249427ef9f0f363ae184855
MNRDev16 <nurroziqin.111@gmail.com>
2021-09-24 17:22:55 +0700

    setDetail data to view

--- SubmissionBFAA2/app/src/main/java/com/mnrdev/android/submissionbfaa2/DetailActivity.kt
+++ SubmissionBFAA2/app/src/main/java/com/mnrdev/android/submissionbfaa2/DetailActivity.kt
@@ -2,7 +2,19 @@ package com.mnrdev.android.submissionbfaa2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.service.controls.ControlsProviderService
+import android.util.Log
+import android.view.View
+import androidx.annotation.StringRes
+import androidx.viewpager2.widget.ViewPager2
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.mnrdev.android.submissionbfaa2.databinding.ActivityDetailBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class DetailActivity : AppCompatActivity() {
 
@@ -13,6 +25,77 @@ class DetailActivity : AppCompatActivity() {
         detailBinding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(detailBinding.root)
 
+        getDetailUser("")
 
+    }
+
+    private fun onCreateTabLayout(){
+        detailBinding.includeViewPager.apply {
+            val sectionsPagerAdapter = SectionsPagerAdapter(this@DetailActivity)
+            val viewPager : ViewPager2 = viewPager
+            viewPager.adapter = sectionsPagerAdapter
+            val tabLayout : TabLayout = tabs
+            TabLayoutMediator(tabLayout,viewPager){tabLayout,position ->
+                tabLayout.text = resources.getString(TAB_TITLE[position])
+
+            }.attach()
+        }
+    }
+    private fun getDetailUser(username : String?){
+        var id = "\"\""
+        if(username != null && username.isNotEmpty()) id = username
+        showLoading(true)
+        val client = ApiConfig.getApiService().getDetail(id)
+        client.enqueue(object : Callback<DetailResponse> {
+            override fun onResponse(
+                call: Call<DetailResponse>,
+                response: Response<DetailResponse>
+            ) {
+                showLoading(false)
+                if (response.isSuccessful) {
+                    val responseBody = response.body()
+                    if (responseBody != null) {
+                        setDetailData(responseBody)
+                    }
+                } else {
+                    Log.e(ControlsProviderService.TAG, "onFailure: ${response.message()}")
+                }
+            }
+            override fun onFailure(call: Call<DetailResponse>, t: Throwable) {
+                showLoading(false)
+                Log.e(ControlsProviderService.TAG, "onFailure: ${t.message}")
+            }
+        })
+    }
+
+    private fun setDetailData(userDetail : DetailResponse){
+        detailBinding.includeDetail.apply {
+            Glide.with(root)
+                .load(userDetail.avatarUrl)
+                .apply(RequestOptions().override(100,100))
+                .into(imageUser)
+            tvName.text = userDetail.name
+            tvNumFollower.text = userDetail.followers.toString()
+            tvNumFollowing.text = userDetail.following.toString()
+            tvNumRepository.text = userDetail.publicRepos.toString()
+            tvLocation.text = userDetail.location
+            tvCompany.text = userDetail.company
+        }
+    }
+
+    private fun showLoading(isLoading: Boolean) {
+        if (isLoading) {
+            detailBinding.progressBarDetail.visibility = View.VISIBLE
+        } else {
+            detailBinding.progressBarDetail.visibility = View.GONE
+        }
+    }
+
+    companion object{
+        @StringRes
+        private val TAB_TITLE = intArrayOf(
+            R.string.following,
+            R.string.followers
+        )
     }
 }
