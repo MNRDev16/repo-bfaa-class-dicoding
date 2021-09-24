a74ad12e176e8e6ea8f338c452f8411c4e9bddd4
MNRDev16 <nurroziqin.111@gmail.com>
2021-09-24 13:55:48 +0700

    add search menu as SearchView to action menu MainActivity

--- SubmissionBFAA2/app/src/main/java/com/mnrdev/android/submissionbfaa2/MainActivity.kt
+++ SubmissionBFAA2/app/src/main/java/com/mnrdev/android/submissionbfaa2/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.mnrdev.android.submissionbfaa2
 
+import android.app.SearchManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.service.controls.ControlsProviderService.TAG
 import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.mnrdev.android.submissionbfaa2.databinding.ActivityMainBinding
 import retrofit2.Call
@@ -21,12 +27,40 @@ class MainActivity : AppCompatActivity() {
         setContentView(mainBinding.root)
 
         mainBinding.rvUserGithub.layoutManager = LinearLayoutManager(this)
-        getSearchData()
+        getSearchData("\"\"")
     }
 
-    private fun getSearchData() {
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.option_menu,menu)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu.findItem(R.id.menu_search).actionView as SearchView
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.queryHint = resources.getString(R.string.search_hint)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                getSearchData(query)
+                //Toast.makeText(this@MainActivity,query, Toast.LENGTH_SHORT).show()
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                getSearchData(newText)
+                return true
+            }
+
+        })
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    private fun getSearchData(username : String?) {
+        var id = "\"\""
+        if(username != null && username.isNotEmpty()) id = username
         showLoading(true)
-        val client = ApiConfig.getApiService().getSearch("sidiqpermana")
+        val client = ApiConfig.getApiService().getSearch(id)
         client.enqueue(object : Callback<SearchResponse> {
             override fun onResponse(
                 call: Call<SearchResponse>,
