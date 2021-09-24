90c76d39e63edc049797117189fecd77ed0aa88c
MNRDev16 <nurroziqin.111@gmail.com>
2021-09-24 16:24:09 +0700

    create main_navigation and fix detail layout

--- SubmissionBFAA2/app/src/main/java/com/mnrdev/android/submissionbfaa2/DetailActivity.kt
+++ SubmissionBFAA2/app/src/main/java/com/mnrdev/android/submissionbfaa2/DetailActivity.kt
@@ -2,10 +2,17 @@ package com.mnrdev.android.submissionbfaa2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mnrdev.android.submissionbfaa2.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
+
+    private lateinit var detailBinding: ActivityDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        detailBinding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(detailBinding.root)
+
+
     }
 }
