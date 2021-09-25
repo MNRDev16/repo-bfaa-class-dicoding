561bf848b486ac498b6599b871f0b4c1f40ad95e
MNRDev16 <nurroziqin.111@gmail.com>
2021-09-25 19:52:43 +0700

    reforming MainActivity

--- SubmissionBFAA2/app/src/main/java/com/mnrdev/android/submissionbfaa2/MainActivity.kt
+++ SubmissionBFAA2/app/src/main/java/com/mnrdev/android/submissionbfaa2/MainActivity.kt
@@ -7,11 +7,11 @@ import android.os.Bundle
 import android.service.controls.ControlsProviderService.TAG
 import android.util.Log
 import android.view.Menu
-import android.view.MenuInflater
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.mnrdev.android.submissionbfaa2.Adapter.UserAdapter
+import com.mnrdev.android.submissionbfaa2.ApiManager.ApiConfig
 import com.mnrdev.android.submissionbfaa2.databinding.ActivityMainBinding
 import retrofit2.Call
 import retrofit2.Callback
@@ -25,73 +25,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
-
-        mainBinding.rvUserGithub.layoutManager = LinearLayoutManager(this)
-        getSearchData("\"\"")
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        val inflater = menuInflater
-        inflater.inflate(R.menu.option_menu,menu)
-
-        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
-        val searchView = menu.findItem(R.id.menu_search).actionView as SearchView
-
-        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
-        searchView.queryHint = resources.getString(R.string.search_hint)
-        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                getSearchData(query)
-                //Toast.makeText(this@MainActivity,query, Toast.LENGTH_SHORT).show()
-                return true
-            }
-
-            override fun onQueryTextChange(newText: String?): Boolean {
-                getSearchData(newText)
-                return true
-            }
-
-        })
-
-        return super.onCreateOptionsMenu(menu)
-    }
-
-    private fun getSearchData(username : String?) {
-        var id = "\"\""
-        if(username != null && username.isNotEmpty()) id = username
-        showLoading(true)
-        val client = ApiConfig.getApiService().getSearch(id)
-        client.enqueue(object : Callback<SearchResponse> {
-            override fun onResponse(
-                call: Call<SearchResponse>,
-                response: Response<SearchResponse>
-            ) {
-                showLoading(false)
-                if (response.isSuccessful) {
-                    val responseBody = response.body()
-                    if (responseBody != null) {
-                        setSearchData(responseBody.items)
-                    }
-                } else {
-                    Log.e(TAG, "onFailure: ${response.message()}")
-                }
-            }
-            override fun onFailure(call: Call<SearchResponse>, t: Throwable) {
-                showLoading(false)
-                Log.e(TAG, "onFailure: ${t.message}")
-            }
-        })
-    }
-
-    private fun setSearchData(searchUsers: List<ItemsItem>) {
-        val adapter = SearchAdapter(searchUsers)
-        mainBinding.rvUserGithub.adapter = adapter
-    }
-    private fun showLoading(isLoading: Boolean) {
-        if (isLoading) {
-            mainBinding.progressBar.visibility = View.VISIBLE
-        } else {
-            mainBinding.progressBar.visibility = View.GONE
-        }
     }
 }
