b9651b6da9ae4f4c34eec8601883b186f86a825d
MNRDev16 <nurroziqin.111@gmail.com>
2021-09-25 19:46:08 +0700

    make viewModel to MainFragment

--- SubmissionBFAA2/app/src/main/java/com/mnrdev/android/submissionbfaa2/Fragment/main/MainFragment.kt
+++ SubmissionBFAA2/app/src/main/java/com/mnrdev/android/submissionbfaa2/Fragment/main/MainFragment.kt
@@ -1,12 +1,18 @@
 package com.mnrdev.android.submissionbfaa2.Fragment.main
 
+import android.app.SearchManager
+import android.content.Context
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.view.*
+import androidx.appcompat.widget.SearchView
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import com.mnrdev.android.submissionbfaa2.Adapter.UserAdapter
+import com.mnrdev.android.submissionbfaa2.ApiManager.response.ItemsItem
+import com.mnrdev.android.submissionbfaa2.MainActivity
 import com.mnrdev.android.submissionbfaa2.R
+import com.mnrdev.android.submissionbfaa2.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
 
@@ -15,18 +21,68 @@ class MainFragment : Fragment() {
     }
 
     private lateinit var viewModel: MainViewModel
-
+    private lateinit var mainFragmentViewBinding: MainFragmentBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        setHasOptionsMenu(true)
+        mainFragmentViewBinding = MainFragmentBinding.inflate(inflater,container,false)
+        return mainFragmentViewBinding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
+        viewModel.searchData.observe(this.viewLifecycleOwner,{
+            setSearchData(it.items)
+        })
+        viewModel.loading.observe(this.viewLifecycleOwner,{
+            showLoading(it)
+        })
+
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.option_menu,menu)
+
+        val searchManager = context?.getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu.findItem(R.id.menu_search).actionView as SearchView
+
+        //val seacrh =
+        //    (context as MainActivity).supportActionBar?.themedContext?.let { SearchView(it) }
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(activity?.componentName))
+        searchView.queryHint = resources.getString(R.string.search_hint)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                if (query != null) {
+                    viewModel.getSearchData(query)
+                }
+                //Toast.makeText(this@MainActivity,query, Toast.LENGTH_SHORT).show()
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if (newText != null) {
+                    viewModel.getSearchData(newText)
+                }
+                return true
+            }
+
+        })
+    }
+
+    private fun setSearchData(searchUsers: List<ItemsItem>) {
+        val adapter = UserAdapter(searchUsers)
+        mainFragmentViewBinding.includeRecycleView.rvUserGithub.adapter = adapter
+    }
+    private fun showLoading(isLoading: Boolean) {
+        if (isLoading) {
+            mainFragmentViewBinding.progressBar.visibility = View.VISIBLE
+        } else {
+            mainFragmentViewBinding.progressBar.visibility = View.GONE
+        }
+    }
 }
--- SubmissionBFAA2/app/src/main/java/com/mnrdev/android/submissionbfaa2/Fragment/main/MainViewModel.kt
+++ SubmissionBFAA2/app/src/main/java/com/mnrdev/android/submissionbfaa2/Fragment/main/MainViewModel.kt
@@ -1,7 +1,46 @@
 package com.mnrdev.android.submissionbfaa2.Fragment.main
 
+import android.service.controls.ControlsProviderService
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.mnrdev.android.submissionbfaa2.ApiManager.ApiConfig
+import com.mnrdev.android.submissionbfaa2.ApiManager.response.SearchResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val _searchData = MutableLiveData<SearchResponse>()
+    val searchData : LiveData<SearchResponse> = _searchData
+
+    private val _loading = MutableLiveData<Boolean>()
+    val loading : LiveData<Boolean> = _loading
+
+    fun getSearchData(username : String) {
+        _loading.value = true
+        val client = ApiConfig.getApiService().getSearch(username)
+        client.enqueue(object : Callback<SearchResponse> {
+            override fun onResponse(
+                call: Call<SearchResponse>,
+                response: Response<SearchResponse>
+            ) {
+                _loading.value = false
+                if (response.isSuccessful) {
+                    val responseBody = response.body()
+                    if (responseBody != null) {
+                        _searchData.value = responseBody!!
+                    }
+                } else {
+                    Log.e(ControlsProviderService.TAG, "onFailure: ${response.message()}")
+                }
+            }
+            override fun onFailure(call: Call<SearchResponse>, t: Throwable) {
+                _loading.value = false
+                Log.e(ControlsProviderService.TAG, "onFailure: ${t.message}")
+            }
+        })
+    }
 }
