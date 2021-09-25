e4e6f0ad3c7d10c44541fc7045d60b1778c3e1e1
MNRDev16 <nurroziqin.111@gmail.com>
2021-09-25 14:09:32 +0700

    manage package

--- SubmissionBFAA2/app/src/main/java/com/mnrdev/android/submissionbfaa2/Fragment/following/FollowingFragment.kt
+++ SubmissionBFAA2/app/src/main/java/com/mnrdev/android/submissionbfaa2/Fragment/following/FollowingFragment.kt
@@ -1,10 +1,11 @@
-package com.mnrdev.android.submissionbfaa2
+package com.mnrdev.android.submissionbfaa2.Fragment.following
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.mnrdev.android.submissionbfaa2.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
---
+++ SubmissionBFAA2/app/src/main/java/com/mnrdev/android/submissionbfaa2/Fragment/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.mnrdev.android.submissionbfaa2.Fragment.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
